f8032d42057aa1035fc4611a66512116aa954cdc
mholwill <m_holwill@hotmail.co.uk>
2020-03-24 19:16:51 +0000

    MVP complete

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
+
 public class Library {
 
     private int capacity;
+    private ArrayList<Book> collection;
 
     public Library(int capacity) {
         this.capacity = capacity;
+        this.collection = new ArrayList<Book>();
     }
 
 
+    public int getBooks() {
+        return this.collection.size();
+    }
 
+    public boolean hasCapacity() {
+        return this.capacity > 0;
+    }
+
+
+    public void addBook(Book book) {
+        if (hasCapacity() == true) {
+            this.collection.add(book);
+            this.capacity -= 1;
+        }
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,13 +1,51 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
     private Library library;
+    private Book book1;
+    private Book book2;
+
 
     @Before
     public void before(){
         library = new Library(2);
+        book1 = new Book("Title 1", "MC", "Horror");
+        book2 = new Book("Title 2", "HEHEHE", "RomCom");
+    }
 
+    @Test
+    public void collectionStartsEmpty(){
+        assertEquals(0, library.getBooks());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(true, library.hasCapacity());
+    }
+
+    @Test
+    public void canAddBook(){
+        library.addBook(book1);
+        assertEquals(1, library.getBooks());
+    }
+
+    @Test
+    public void checkCapacityAfterBookAdded(){
+        library.addBook(book1);
+        assertEquals(1, library.getCapacity());
+    }
+
+
+    @Test
+    public void checkNoCapacityLeft(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book1);
+        assertEquals(false, library.hasCapacity());
     }
 
 }
